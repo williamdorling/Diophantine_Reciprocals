6e34d2c39a9a8c17961b36c6aabac735b26a5455
William Dorling <williamdorling@hotmail.com>
2023-07-18 16:43:07 +0100

    add method to count pairwise factorisations of inteer input

--- Problems_108_and_110.js
+++ Problems_108_and_110.js
@@ -1,3 +1,16 @@
+// Problem 108:
+// In the following equation x, y, and n are positive integers.
+//  1/x + 1/y = 1/n
+// For n=4 there are exactly three distinct solutions:
+//  1/5 +1/20 = 1/4
+//  1/6 +1/12 = 1/4
+//  1/8 + 1/8 = 1/4
+// What is the least value of n for which the number of distinct solutions exceeds one-thousand?
+ 
+ 
+
+
+// method to count number of divisors of input integer n
 const divisorCount = (n) => {
     let count = 0;
     for (i=1; i< n**0.5; i++){
@@ -22,4 +35,20 @@ const divisorCount2 = (n) => {
   return count;
 }
 
+
+// method to count number of pairs (a,b) such that a*b = n for input integer n
+const pairwiseFactorisations = (n) => {
+    let count = 0;
+    for (i=1; i< n**0.5; i++){
+        if (n % i === 0){
+            count ++;
+        }
+    }
+    if (n**0.5 % 1 === 0){
+        count++;
+    }
+    return count;
+}
+
+// console.log(pairwiseFactorisations(1260**2));
 
